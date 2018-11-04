df1a4d10dcd6ac77c4415ff386753af39575647a
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 09:18:05 +0300

    Add a form element

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,6 +2,10 @@ import React,{Component} from 'react';
 
 class SearchBar extends Component{
   render(){
-    
+    return(
+      <form className="input-group">
+      
+      </form>
+    );
   }
 }
