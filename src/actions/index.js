a4c7a954156216590ba29305dd0a1dc006868e70
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 10:58:48 +0300

    Add const FETCH_WEATHER

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 const API_KEY = '03d17752ca362bc60ca7df94aac228a6';
 const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
 export default function fetchWeather(city){
   const url =`${ROOT_URL}&${city},us`;
   const request = axios.get(url);
+
 
 }
