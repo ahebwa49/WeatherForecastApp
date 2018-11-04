63574a267b8a5ccfb1ca06994276bc5cd97723a1
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 09:57:19 +0300

    bind context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -6,6 +6,7 @@ class SearchBar extends Component{
 
     this.state = {term: ''};
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 onInputChange(event){
   this.setState({term: event.target.value});
