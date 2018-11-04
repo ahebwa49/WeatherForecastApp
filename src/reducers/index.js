0d9ed1b6ca6c152ac9398e14c433ec9310ff2610
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 11:38:18 +0300

    map weather property to weather reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import WeatherReducer from './reducer_weather_list';
 
 const rootReducer = combineReducers({
-
+  weather: WeatherReducer
 });
 
 export default rootReducer;
