1df1fb3d1b072df278e517fa5f73fa201289cb33
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 09:26:40 +0300

    Initialize state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,11 @@
 import React,{Component} from 'react';
 
 class SearchBar extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {term: ''};
+  }
   render(){
     return(
       <form className="input-group">
