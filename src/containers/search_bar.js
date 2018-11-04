d669c8325b07880a0582b6fa1667a4bab3e42ee8
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 09:21:53 +0300

    Add an input element to form

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,7 +4,10 @@ class SearchBar extends Component{
   render(){
     return(
       <form className="input-group">
-      
+        <input
+        className="form-control"
+        placeholder="Enter yout favorite city to get forecast for next five days"
+        />
       </form>
     );
   }
