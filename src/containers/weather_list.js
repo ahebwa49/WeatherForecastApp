8b5a0bd8786ec89f0e8efad883ab3871a26bf6f1
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 11:48:29 +0300

    return a table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,20 @@ import React, {Component} from 'react';
 
 class WeatherList extends Component{
   render(){
-    
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temparature</th>
+            <th>Humidity</th>
+            <th>Pressure</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    );
   }
 }
