053d5c4d9db05334aa0eb7aa57446eff96cd4769
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 11:34:30 +0300

    Add weather reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import WeatherReducer from './reducer_weather_list';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+
 });
 
 export default rootReducer;
