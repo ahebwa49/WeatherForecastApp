dccf21cfd474c1d1cf7c8cee619286b050419b27
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-11-04 10:50:17 +0300

    Add a url

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,5 @@ const API_KEY = '03d17752ca362bc60ca7df94aac228a6';
 const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export default function fetchWeather(city){
-
+  const url =`${ROOT_URL}&${city},us`;
 }
